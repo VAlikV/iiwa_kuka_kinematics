f9f53a96b4f398c1469b12f42a481cc42ccbb5c2
VAlikV <alik.valiullin2002@yandex.ru>
2024-09-19 23:48:53 +0300

    add visual

---
+++ visual.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# f = open("joints.txt", "r")
+data = np.genfromtxt("joints.txt", delimiter="  ", dtype=float)
+
+data = np.insert(data,0,[0,0,0],axis=1)
+
+# data = np.reshape(data,(8,3))
+print(data)
+
+
+fig = plt.figure()
+
+ax = plt.axes(projection='3d')
+plt.axis('equal')
+
+ax.set_xlim3d([-1000, 1000])
+ax.set_ylim3d([-1000, 1000])
+ax.set_zlim3d([0, 1300])
+ax.set_xlabel("X")
+ax.set_ylabel("Y")
+
+ax.plot3D(data[0], data[1], data[2], 'red')
+
+ax.scatter3D(data[0], data[1], data[2])
+
+plt.show()
