f0e77b454ad156e39be751a4581c561f36ed5e80
ValikV <alik.valiullin2002@yandex.ru>
2025-01-24 19:31:39 +0300

    complite example

--- main.cpp
+++ main.cpp
@@ -12,6 +12,15 @@ int main(int argc, char** argv)
 
     Eigen::Array<double, N_JOINTS,1> q;
     q << 0*M_PI/180, 
+        75*M_PI/180, 
+        90*M_PI/180, 
+        -40*M_PI/180, 
+        -20*M_PI/180,
+        30*M_PI/180,
+        10*M_PI/180;
+
+    Eigen::Array<double, N_JOINTS,1> q_ik;
+    q_ik << 0*M_PI/180, 
         100*M_PI/180, 
         75*M_PI/180, 
         -25*M_PI/180, 
@@ -19,12 +28,27 @@ int main(int argc, char** argv)
         10*M_PI/180,
         0*M_PI/180;
 
+    Eigen::Array<double,3,1> position_ik(0.861414, 0.224974, 0.132113);
+    Eigen::Matrix<double,3,3> rotation_ik;
+    rotation_ik << -0.602578,  -0.16522,   0.78077,
+                    0.425703,   0.76098,  0.489578,
+                    -0.675038,  0.627385, -0.388216;
+
+    int ik_result;
+    clock_t t;
+    double time_taken;
+
 
     std::cout << "Hello, I'm kinematics solvers test!" << std::endl << std::endl;
 
     std::cout << "Angles for tests: " << q.transpose() << std::endl << std::endl; 
 
+    std::cout << "Position for tests: " << position_ik.transpose() << std::endl << std::endl; 
+    std::cout << "Matrix for tests: " << rotation_ik << std::endl << std::endl; 
+
     // ---------------------------------------------------------- Инициализация KDL
+
+    std::cout << "===========================================================" << std::endl;
 
     std::cout << "1) KDL solver" << std::endl << std::endl;
 
@@ -38,12 +62,37 @@ int main(int argc, char** argv)
     position = kinematic_KDL.getPositionVector();
     rotation = kinematic_KDL.getRotationMatrix();
 
-    std::cout << "Положение: " << position.transpose() << std::endl;
-    std::cout << "Матрица: " << rotation << std::endl << std::endl;
+    std::cout << "Position: " << position.transpose() << std::endl;
+    std::cout << "Matrix: " << rotation << std::endl << std::endl;
 
-    std::cout << "Angles for tests: " << q.transpose() << std::endl << std::endl; 
+    t = clock();
+
+    kinematic_KDL.setPositionVector(position_ik);
+    kinematic_KDL.setRotationMatrix(rotation_ik);
+
+    ik_result = kinematic_KDL.IK();
+
+    q_ik = kinematic_KDL.getQDeg();
+
+    t = clock() - t;
+
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
+    std::cout << "Время расчета: " << time_taken*1000 << std::endl;
+
+    kinematic_KDL.setQDeg(q_ik);
+    kinematic_KDL.FK();
+
+    position = kinematic_KDL.getPositionVector();
+    rotation = kinematic_KDL.getRotationMatrix();
+
+    std::cout << "Result: " << ik_result << std::endl;
+    std::cout << "Angles: " << q_ik.transpose() << std::endl;
+    std::cout << "IK position: " << position.transpose() << std::endl;
+    std::cout << "IK rotation: " << rotation << std::endl << std::endl;
 
     // ---------------------------------------------------------- Инициализация SNS
+
+    std::cout << "===========================================================" << std::endl;
 
     std::cout << "2) SNS solver" << std::endl;
 
@@ -53,7 +102,9 @@ int main(int argc, char** argv)
 
     // ---------------------------------------------------------- Инициализация Pinocchio
 
-    std::cout << "\n\n3) Pinocchio solver" << std::endl << std::endl;
+    std::cout << "\n\n===========================================================" << std::endl;
+
+    std::cout << "3) Pinocchio solver" << std::endl << std::endl;
 
     PinKinematic* Pin_solver = new PinKinematic("../robots/iiwa.urdf");
 
@@ -65,12 +116,39 @@ int main(int argc, char** argv)
     position = kinematic_Pin.getPositionVector();
     rotation = kinematic_Pin.getRotationMatrix();
 
-    std::cout << "Положение: " << position.transpose() << std::endl;
-    std::cout << "Матрица: " << rotation << std::endl << std::endl;
+    std::cout << "Position: " << position.transpose() << std::endl;
+    std::cout << "Matrix: " << rotation << std::endl << std::endl;
+
+    t = clock();
+
+    kinematic_Pin.setPositionVector(position_ik);
+    kinematic_Pin.setRotationMatrix(rotation_ik);
+
+    ik_result = kinematic_Pin.IK();
+
+    q_ik = kinematic_Pin.getQDeg();
+
+    t = clock() - t;
+
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
+    std::cout << "Время расчета: " << time_taken*1000 << std::endl;
+
+    kinematic_Pin.setQDeg(q_ik);
+    kinematic_Pin.FK();
+
+    position = kinematic_Pin.getPositionVector();
+    rotation = kinematic_Pin.getRotationMatrix();
+
+    std::cout << "Result: " << ik_result << std::endl;
+    std::cout << "Angles: " << q_ik.transpose() << std::endl;
+    std::cout << "IK position: " << position.transpose() << std::endl;
+    std::cout << "IK rotation: " << rotation << std::endl << std::endl;
 
     // ---------------------------------------------------------- Инициализация Drake
 
-    std::cout << "\n\n4) Drake solver" << std::endl << std::endl;
+    std::cout << "===========================================================" << std::endl;
+
+    std::cout << "4) Drake solver" << std::endl << std::endl;
 
     DrakeKinematic* Drake_solver = new DrakeKinematic("../robots/iiwa.urdf");
 
@@ -82,82 +160,37 @@ int main(int argc, char** argv)
     position = kinematic_Drake.getPositionVector();
     rotation = kinematic_Drake.getRotationMatrix();
 
-    std::cout << "Положение: " << position.transpose() << std::endl;
-    std::cout << "Матрица: " << rotation << std::endl << std::endl;
+    std::cout << "\nPosition: " << position.transpose() << std::endl;
+    std::cout << "Matrix: " << rotation << std::endl << std::endl;
+
+    t = clock();
+
+    kinematic_Drake.setPositionVector(position_ik);
+    kinematic_Drake.setRotationMatrix(rotation_ik);
+
+    ik_result = kinematic_Drake.IK();
+
+    q_ik = kinematic_Drake.getQDeg();
+
+    t = clock() - t;
+
+    time_taken = ((double)t)/CLOCKS_PER_SEC;
+    std::cout << "Время расчета: " << time_taken*1000 << std::endl;
+
+    kinematic_Drake.setQDeg(q_ik);
+    kinematic_Drake.FK();
+
+    position = kinematic_Drake.getPositionVector();
+    rotation = kinematic_Drake.getRotationMatrix();
+
+    std::cout << "Result: " << ik_result << std::endl;
+    std::cout << "Angles: " << q_ik.transpose() << std::endl;
+    std::cout << "IK position: " << position.transpose() << std::endl;
+    std::cout << "IK rotation: " << rotation << std::endl << std::endl;
+
 
     // ---------------------------------------------------------- Задание углов
 
     
-
-
-   
-
-
-
-    // // KDLKinematic* KDL_solver = new KDLKinematic();
-    // SNSKinematic* SNS_solver = new SNSKinematic(sns_ik::VelocitySolveType::SNS);
-
-    // Kinematic kinematic = Kinematic(SNS_solver);
-
-    // // Eigen::Array<double,N_JOINTS,3> a = kinematic.getJointPose();
-    // // std::cout << a << "\n\n";
-
-    // Eigen::Matrix<double,3,3> rotation;
-
-    // rotation = kinematic.getRotationMatrix();
-
-    // // rotation << 0, 0, 0,
-    // //             0, 0, 0,
-    // //             0, 0, 0;
-
-    // Eigen::Array<double,3,1> position; 
-
-    // position = kinematic.getPositionVector();
-
-    // std::cout << std::endl;
-    // std::cout << "Начальное положение: " << std::endl << (double)position(0)*100 << " " << (double)position(1)*100 << " " << (double)position(2)*100 << std::endl;
-    // std::cout << std::setprecision(4) << "Начатльная матрица: " << std::endl << rotation << std::endl;
-
-    // Eigen::Array<double,N_JOINTS,1> q;
-
-    // double pose[] {0,0,0};
-    // double q1[] {0,0,0,0,0,0,0};
-
-    // std::cout << std::endl;
-    // // std::cin >> q1[0] >> q1[1] >> q1[2] >> q1[3] >> q1[4] >> q1[5] >> q1[6];
-    // // q << q1[0], q1[1], q1[2], q1[3], q1[4], q1[5], q1[6];
-
-    // std::cin >> pose[0] >> pose[1] >> pose[2];
-    // position << pose[0]/100, pose[1]/100, pose[2]/100;
-
-    // clock_t t = clock();
-    // int state = kinematic.IK(rotation, position);
-    // t = clock() - t;
-    // double time_taken = ((double)t)/CLOCKS_PER_SEC;
-    // std::cout << "Время расчета: " << time_taken*1000 << std::endl;
-
-    // // kinematic.setQDeg(q);
-    // // kinematic.FK();
-    
-    // std::cout << std::endl << "Статус: " << state << std::endl;
-
-    // Eigen::Array<double,3,1> j = kinematic.getPositionVector();
-    // Eigen::Array<double,3,3> i = kinematic.getRotationMatrix();
-
-    // Eigen::Array<double,N_JOINTS,1> k = kinematic.getQDeg();
-
-    // std::cout << std::setprecision(4) << "Положение: " << std::endl << (double)j(0)*100 << " " << (double)j(1)*100 << " " << (double)j(2)*100 << std::endl;
-    // std::cout << std::setprecision(4) << "Матрица: " << std::endl << i << std::endl;
-    // std::cout << std::setprecision(4) << "Q: " << (double)k(0) << " " << (double)k(1) << " " << (double)k(2) << " " << (double)k(3) << " " << (double)k(4) << " " << (double)k(5) << " " << (double)k(6) << std::endl;
-
-    // position << -0.45, 0.45, 0.3;
-
-    // Eigen::Array<double,N_JOINTS,3> b = kinematic.getJointPose();
-    // std::cout << b << std::endl;
-
-    // saveMatrix("joints.txt",a);
-    // saveMatrix("joints1.txt",b);
-
     return 0;
-
 }
